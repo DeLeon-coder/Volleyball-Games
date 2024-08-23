e8299490f40b4e335f89beca3ba39f89846dbfc4
De Leon <kayllester.o.DeLeon@gmail.com>
2024-08-23 18:30:47 +1200

    Update#1 teams.php

--- Volleyball/teams.php
+++ Volleyball/teams.php
@@ -1,57 +1,107 @@
+<?php
+// Include the database connection
+include('connect.inc');
+
+// Query the team_table
+$query = "SELECT * FROM team_table";
+$result = $conn->query($query);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Something</title>
-    <link rel="stylesheet" href="home.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teams List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
-
-
 <body>
-    
-    <h1> Volleyball Roster </h1>
-    <nav class=navbar>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="teams.php">Teams</a></li>
-        </ul>
-    </nav>    
-    <br>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="Home.php">Volleyball</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class= "navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="Home.php">Home<span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="teams.php">Teams</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Update
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="Quarter_update.php">Quarter Games</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Insert
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="Quarter_insert.php">Quarter Games</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Delete
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <h1 class="mt-5">Teams List</h1>
+        <table class="table table-striped mt-4 text-white">
+            <thead>
+                <tr>
+                    <th>Team ID</th>
+                    <th>Team Name</th>
+                    <th>Venue</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Check if there are results
+                if ($result->num_rows > 0) {
+                    // Fetch data row by row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row['team_id'] . "</td>";
+                        echo "<td>" . $row['team_name'] . "</td>";
+                        echo "<td>" . $row['venue_id'] . "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>No teams found</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>     
 </body>
 </html>
 
 <?php
-include_once('connect.inc');
-
-// Make sure $data_result is initialized
-$data_result = array();
-
-// Query to select all from Team_Table
-$query = "SELECT * FROM Team_Table";
-$result = $conn->query($query);
-
-if ($result) {
-    // Fetch all results into $data_result
-    while ($row = $result->fetch_assoc()) {
-        $data_result[] = $row;
-    }
-
-    // Check if there are results to display
-    if (!empty($data_result)) {
-        foreach ($data_result as $row) {
-            echo 'Team Number: ' . $row['team_id'] . '<br>';
-            echo 'Team Name: ' . $row['team_name'] . '<br>';
-            echo 'Venue: ' . $row['venue_id'] . '<br>';
-            echo '<hr>'; // Add a horizontal rule for better readability
-        }
-    } else {
-        echo 'No teams found.';
-    }
-} else {
-    echo 'Query failed: ' . $conn->error;
-}
-
+// Close the database connection
+$conn->close();
 ?>
